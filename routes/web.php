f57bbcdba3cf62f21ade7c97714680de7f313abd
holiq <holiq.ibrahim376@gmail.com>
2020-08-25 15:23:47 +0700

    update route and add controller

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'BlogController@index')->name('index');
+Route::get('/category/{slug}', 'BlogController@category')->name('blog.category');
+Route::get('/post/{post:slug}', 'PostController@show')->name('post.show');
+Route::post('/post/comment', 'CommentController@store')->name('post.comment.store');
+Route::post('/summernote_upload', 'SummernoteController@store')->name('summernote.upload');
+Route::post('/summernote/delete', 'SummernoteController@destroy')->name('summernote.delete');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/user', 'BlogController@user')->name('blog.user');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('/', 'AdminController@index')->name('admin.index');
+    Route::get('/', 'AdminController@showLoginForm')->name('admin.login');
+    Route::post('/', 'AdminController@login')->name('admin.login');
+    Route::post('/', 'AdminController@logout')->name('admin.logout');
+    Route::get('/', 'AdminController@index')->name('admin.index');
+    Route::resource('/category', 'CategoryController')->except([
+		'show',
+	]);
+	Route::resource('/post', 'PostController');
+	Route::resource('/comment', 'CommentController');
+});
