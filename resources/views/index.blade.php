beaa8ffd1ee70a3ac27c9a2bbc67710f708876c3
holiq <holiq.ibrahim376@gmail.com>
2020-09-22 06:40:11 +0700

    fix layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,101 +1,58 @@
 @extends('layouts.app')
-
 @section('content')
-        <!-- component -->
-<main class="py-4">
-    <div class="px-4">
+<main class="p-4">
         <div class="flex flex-wrap">
-            <div class="w-full md:w-2/3 mb-8 px-2">
+            <div class="w-full md:w-2/3 mb-4">
                 <div class="grid md:grid-cols-2 gap-4">
                     @foreach($post as $row)
-                    <div class="bg-white rounded overflow-hidden shadow hover:shadow-lg transition duration-100 transform hover:-translate-y-1">
-                        <img class="h-56 w-full object-cover object-center" src="/images/post/{{ $row->thumbnail }}" alt="">
+                    <div class="bg-white rounded shadow hover:shadow-lg transition duration-100 transform hover:-translate-y-1">
+                        <img class="h-56 w-full object-cover object-center" src="/images/post/{{ $row->thumbnail }}" alt="{{ $row->title }}">
                         <div class="p-4 h-auto">
                             <a class="inline-block bg-indigo-700 py-1 px-2 rounded mb-2 text-white text-xs uppercase" href="{{ route('blog.category', $row->category->slug) }}">{{ $row->category->name }}</a>
                             <a href="{{ route('post', $row->slug) }}" class="block text-gray-900 font-semibold mb-2 text-lg md:text-base lg:text-lg">
-                                {{ Str::limit($row->title, 40) }}
+                            {{ Str::limit($row->title, 40) }}
                             </a>
                             <div class="text-gray-700 text-sm leading-relaxed block md:text-xs lg:text-sm">
                                 {{ Str::limit(strip_tags($row->content), 200) }}
                             </div>
                             <div class="flex justify-left flex-wrap mt-2 mb-3">
                                 @foreach($row->tags as $tag)
-                                <a class="inline bg-gray-200 py-1 px-2 mr-1 mb-2 rounded-full text-xs lowercase text-gray-700" href="{{ route('blog.tag', $tag->slug) }}">#{{ $tag->name }}</a>
+                                <a class="inline bg-gray-200 py-1 px-2 mr-1 mb-2 rounded text-xs lowercase text-gray-700" href="{{ route('blog.tag', $tag->slug) }}">#{{ $tag->name }}</a>
                                 @endforeach
                             </div>
-                            <div class="flex text-gray-700 text-sm">
+                            <div class="flex text-gray-700 text-sm md:text-xs">
                                 <div>Posted by <span class="font-semibold">{{ $row->user->name}} </span></div>
                                 <span class="px-2">{{ $row->created_at->format('M d, Y') }}</span>
                                 <span class="text-red-500">{{ $row->featured ? 'Featured':'' }}</span>
                             </div>
                             <div class="mt-4 flex items-center">
                                 <div class="flex mr-2 text-gray-700 text-sm mr-3">
-                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                    <svg fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-1" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                     </svg>
-                                    <span>244</span>
+                                    <span>{{ $row->views }}</span>
                                 </div>
                                 <div class="flex mr-2 text-gray-700 text-sm mr-5">
                                     <svg fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-1" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"/>
                                     </svg>
-                                    <span>25</span>
+                                    <span>{{ $row->countComment->count() }}</span>
                                 </div>
                                 <div class="flex mr-2 text-gray-700 text-sm mr-4">
                                     <svg fill="none" viewBox="0 0 24 24" class="w-4 h-4 mr-1" stroke="currentColor">
-                                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"/>
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
                                     </svg>
                                     <span>share</span>
                                 </div>
-                             </div>
+                            </div>
                         </div>
-                    </div>
+                    </div>                        
                     @endforeach
                 </div>
                 {{ $post->links('vendor.pagination.tailwind') }}
             </div>
             <div class="w-full md:w-1/3 mb-4 px-2">
-                <div class="bg-white rounded overflow-hidden shadow hover:shadow-lg">
-                    <div class="p-4 h-auto">
-                        <h3 class="font-semibold mb-3">Popular Post</h3>
-                        <div class="flex flex-wrap">
-                            <div class="w-2/5 mb-4">
-                                <img class="h-32 w-full object-cover object-center" src="https://images.unsplash.com/photo-1457282367193-e3b79e38f207?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1654&q=80" alt="">
-                            </div>
-                            <div class="w-3/5 pl-3">
-                                <a href="#" class="block text-blue-500 hover:text-blue-600 font-semibold mb-2 text-lg md:text-base lg:text-lg">
-                                    Woman standing under blue sky
-                                </a>
-                                <div class="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quidem blanditiis unde asperiores?
-                                </div>
-                            </div>
-                        </div>
-                        <div class="flex flex-wrap">
-                            <div class="w-2/5 mb-4">
-                                {{-- <img class="h-32 w-full object-cover object-center" src="https://images.unsplash.com/photo-1457282367193-e3b79e38f207?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1654&q=80" alt=""> --}}
-                                <div class="h-32 bg-gray-400"></div>
-                            </div>
-                            <div class="w-3/5 pl-3">
-                                <a href="#" class="block text-blue-500 hover:text-blue-600 font-semibold mb-2 text-lg md:text-base lg:text-lg">
-                                    Woman standing under blue sky
-                                </a>
-                                <div class="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quidem blanditiis unde asperiores?
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="px-4 pb-4 h-auto">
-                        <h3 class="font-semibold mb-3">Category</h3>
-                        <div class="flex justify-left flex-wrap">
-                            @foreach($category as $row)
-                            <a class="bg-gray-300 py-1 px-2 rounded text-xs lowercase text-gray-700 mr-1 mb-1" href="#">{{ $row->name }}</a>
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
+                @include('partials.sidebar')
             </div>
         </div>
     </div>
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,19 +1,5 @@
-<div class="bg-white rounded overflow-hidden shadow mb-4">
-    <div class="p-4 h-auto">
-        <h4 class="antialiased text-gray-700 font-semibold tracking-wide mb-2">Get the latest update</h4>
-        <h5 class="antialiased text-600 tracking-tight">Subscribe now to get the latest events and news.</h5>
-        <form action="#" method="post">
-            <div class="mt-3 flex flex-row flex-wrap">
-                @csrf
-                @method('post')
-                <input type="text"class="w-2/3 bg-gray-100 rounded-l text-gray-600 text-sm leading-normal resize-none py-2 px-3 font-medium placeholder-gray-400 focus:outline-none" placeholder="email@example.com"/>
-                <input type="submit" class="p-1 w-1/3 bg-indigo-500 rounded-r text-white transition duration-300 ease-in-out focus:outline-none focus:shadow-outline hover:bg-indigo-600 font-medium" value="Subscribe">
-            </div>
-        </form>
-    </div>
-</div>
-<div class="bg-white rounded overflow-hidden shadow mb-4">
-    <div class="p-4 h-auto">
+<div class="bg-white rounded overflow-hidden shadow p-4 mb-4">
+    <div class="h-auto mb-4">
         <h3 class="font-semibold mb-3">Popular Post</h3>
         @foreach($popular as $row)
         <div class="flex flex-wrap">
@@ -31,7 +17,33 @@
         </div>
         @endforeach
     </div>
-    <div class="px-4 pb-4 h-auto">
+    <div class="h-auto mb-4">
+        <div class="flex justify-between">
+            <h3 class="font-semibold mb-3">Featured Post</h3>
+            <a href="#" class="text-gray-700 font-medium text-base">More</a>
+        </div>
+        @foreach($featured as $row)
+        <div class="flex flex-wrap">
+            <div class="w-2/5 md:w-2/6 mb-4">
+                <img class="w-full h-32 md:h-full object-cover object-center" src="/images/post/{{ $row->thumbnail }}" alt="{{ $row->title }}">
+            </div>
+            <div class="w-3/5 md:w-4/6 pl-3">
+                <a href="{{ route('post', $row->slug) }}" class="block text-gray-900 font-semibold mb-2 text-lg md:text-base lg:text-lg">
+                    {{ Str::limit($row->title, 35) }}
+                </a>
+                <div class="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
+                    {{ Str::limit(strip_tags($row->content), 100) }}
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    
+    
+    
+</div>
+<div class="bg-white rounded overflow-hidden shadow p-4 mb-4">
+    <div class="h-auto mb-4">
         <h3 class="font-semibold mb-3">Category</h3>
         <div class="flex justify-left flex-wrap">
             @foreach($category as $row)
@@ -39,9 +51,7 @@
             @endforeach
         </div>
     </div>
-</div>
-<div class="bg-white rounded overflow-hidden shadow mb-4">
-    <div class="p-4 h-auto">
+    <div class="h-auto mb-4">
         <h3 class="font-semibold mb-3">Tags</h3>
         <div class="flex justify-left flex-wrap break-all">
             @foreach($tags as $row)
@@ -49,4 +59,22 @@
             @endforeach
         </div>
     </div>
+    
+    
+</div>
+<div class="bg-white rounded overflow-hidden shadow p-4 mb-4">
+    <div class="h-auto mb-4">
+        <h3 class="antialiased text-gray-700 font-semibold tracking-wide mb-2">Get the latest update</h3>
+        <h5 class="antialiased text-600 tracking-tight">Subscribe now to get the latest events and news.</h5>
+        <form action="#" method="post">
+            <div class="mt-3 flex flex-row flex-wrap">
+                @csrf
+                @method('post')
+                <input type="text"class="w-2/3 bg-gray-100 rounded-l text-gray-600 text-sm leading-normal resize-none py-2 px-3 font-medium placeholder-gray-400 focus:outline-none" placeholder="email@example.com"/>
+                <input type="submit" class="p-1 w-1/3 bg-indigo-500 rounded-r text-white transition duration-300 ease-in-out focus:outline-none focus:shadow-outline hover:bg-indigo-600 font-medium" value="Subscribe">
+            </div>
+        </form>
+    </div>
+    
+    
 </div>
